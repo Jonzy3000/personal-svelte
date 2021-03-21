9db7c4d43ac95f59d82764c9a6deb1359e1ce7b4
Matt Jones <matt.r.jones1995@gmail.com>
2021-03-21 09:59:41 +0000

    Setup using svelte kit and netlify adapters

---
+++ src/routes/index.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<main class="flex flex-col items-center p-4">
+	<h1 class="uppercase text-7xl my-16 mx-auto text-red-500 max-w-xs sm:max-w-none">Hello world!</h1>
+
+	<Counter />
+
+	<p class="my-8 mx-auto leading-5 max-w-xs sm:max-w-none">Visit <a class="text-red-500" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+			/*
+
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+		line-height: 1.1;
+		margin: 4rem auto;
+		max-width: 14rem;
+	}
+
+	p {
+		max-width: 14rem;
+		margin: 2rem auto;
+		line-height: 1.35;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+
+		p {
+			max-width: none;
+		}
+	} */
+</style>
