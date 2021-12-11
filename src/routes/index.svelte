951ee1a8f3aae2c434461d2d33b7a4680888c42a
Matt Jones <matt@boclips.com>
2021-12-11 12:45:19 +0000

    Add tailwind 3.0

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" context="module">
   import type { Load } from '@sveltejs/kit';
-  export const load: Load = async ({ page, fetch, session, context }) => {
+  export const load: Load = async ({ fetch }) => {
     const res = await fetch('/current-song');
 
     if (res.ok) {
@@ -19,16 +19,12 @@
   export let currentSong;
 </script>
 
-<main class="flex flex-col items-center p-4">
-  <h1
-    class="uppercase text-7xl my-16 mx-auto text-red-500 max-w-xs sm:max-w-none"
-  >
-    Hello world!
-  </h1>
+<main class="p-4 mx-auto max-w-xs sm:max-w-none">
+  <h1 class="uppercase text-7xl my-16 text-red-500 ">Hello world!</h1>
 
   <Counter />
 
-  <p class="my-8 mx-auto leading-5 max-w-xs sm:max-w-none">
+  <p class="my-8 leading-5">
     Visit <a class="text-red-500" href="https://svelte.dev">svelte.dev</a> to learn
     how to build Svelte apps.
   </p>
@@ -41,43 +37,3 @@
     {/if}
   </div>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-  /*
-
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	} */
-</style>
