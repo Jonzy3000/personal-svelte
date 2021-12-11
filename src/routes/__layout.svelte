cbef41e6ba1bab992d190e71ff59c98175e69fa7
Matt Jones <matt@boclips.com>
2021-12-11 16:13:03 +0000

    Add a footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,19 @@
   import '../app.css';
 </script>
 
-<slot />
+<div class="flex flex-col items-center p-8 mx-auto max-w-lg w-full h-full">
+  <slot />
+  <hr />
+  <footer
+    class="flex flex-col items-center mt-auto pt-4 text-gray-400 border-t w-full"
+  >
+    <div>
+      Built with {' '}
+      <a
+        class="text-red-600 hover:underline"
+        href="https://kit.svelte.dev"
+        target="#">svelte kit</a
+      >
+    </div>
+  </footer>
+</div>
