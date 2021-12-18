58c89d5fa4ce20687247fcb57b01df30f1063bf6
Matt Jones <matt@boclips.com>
2021-12-18 14:39:29 +0000

    Cache access token until it expires

--- src/lib/server/spotify/spotify.ts
+++ src/lib/server/spotify/spotify.ts
@@ -6,16 +6,29 @@ const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID as string;
 const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET as string;
 const refresh_token = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN as string;
 
-console.log(client_id);
-
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const MY_TOP_TRACKS = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-
 const spotifyApi = new SpotifyWebApi();
 
+let cached = global['spotify'];
+
+if (!cached) {
+  cached = global['spotify'] = {
+    access_token: null,
+    token_type: null,
+    expires_in: null,
+    scope: null,
+    time_created: null,
+  };
+}
+
 const getAccessToken = async () => {
+  if (cached.access_token && !hasTokenExpired()) {
+    console.log('Using cached token');
+    return Promise.resolve(cached);
+  }
+
+  console.log('Fetching spotify token');
   const response = await fetch(TOKEN_ENDPOINT, {
     method: 'POST',
     headers: {
@@ -28,11 +41,19 @@ const getAccessToken = async () => {
     }),
   });
 
-  return response.json();
+  return response.json().then((json) => {
+    const token = { time_created: Date.now(), ...json };
+    cached = token;
+    return token;
+  });
 };
 
 export async function getAuthenticatedSpotifyApi(): Promise<SpotifyWebApi> {
   const { access_token } = await getAccessToken();
   spotifyApi.setAccessToken(access_token);
   return spotifyApi;
+}
+
+function hasTokenExpired() {
+  return cached.time_created + cached.expires_in >= Date.now() - 1000;
 }
