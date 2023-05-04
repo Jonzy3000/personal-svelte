77a97288a54a60c40870ac051919682f26b4b417
jonzy3000 <jonzy_3000@hotmail.co.uk>
2023-05-04 17:38:55 +1200

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import Footer from '$components/Footer.svelte';
   import Nav from '$components/Nav.svelte';
   import '../app.css';
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div
