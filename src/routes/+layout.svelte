5ff57680d2852bf311fde82b4768e98fbcfdcaf9
jonzy3000 <jonzy_3000@hotmail.co.uk>
2023-07-24 13:37:06 +1200

    Add font declaration to <head> and add a display strategy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,40 @@
   inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<div data-sveltekit-preload-data class="min-h-screen">
-  <Nav />
-  <main class="w-full mx-auto">
-    <slot />
-  </main>
-  <Footer />
-</div>
+<svelte:head>
+  <style>
+    @font-face {
+      font-family: 'Archivo';
+      src: url('/archivo-latin-400-normal.woff2');
+      font-display: swap;
+    }
+
+    @font-face {
+      font-family: 'Archivo';
+      font-weight: 800;
+      src: url('/archivo-latin-800-normal.woff2');
+      font-display: swap;
+    }
+
+    @font-face {
+      font-family: 'Archivo';
+      font-weight: 500;
+      src: url('/archivo-latin-500-normal.woff2');
+      font-display: swap;
+    }
+
+    html,
+    body,
+    #svelte {
+      width: 100%;
+      height: 100%;
+      font-family: 'Archivo', sans-serif;
+    }
+  </style>
+</svelte:head>
+
+<Nav />
+<main class="w-full mx-auto">
+  <slot />
+</main>
+<Footer />
