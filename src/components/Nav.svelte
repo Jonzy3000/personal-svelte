4d604353afa1b128159be42381b1aba0b67bb1c6
jonzy3000 <matt.r.jones1995@gmail.com>
2026-05-29 10:45:39 +0100

    feat: use simple-icons for GitHub, inline SVG for LinkedIn (dropped from simple-icons)

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,7 @@
+<script>
+  import { SiGithub } from '@icons-pack/svelte-simple-icons';
+</script>
+
 <nav
   class="flex justify-center border-b-2 border-black bg-white px-4 uppercase md:px-0"
 >
@@ -13,24 +17,7 @@
         href="https://github.com/Jonzy3000/personal-svelte"
         aria-label="Matt Jones' github"
       >
-        <!-- Github icon (lucide) -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          aria-hidden="true"
-        >
-          <path
-            d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
-          />
-          <path d="M9 18c-4.51 2-5-2-7-2" />
-        </svg>
+        <SiGithub size={24} />
       </a>
       <a
         class="rounded border-2 border-black bg-blue-100 p-1 shadow-[0px_2px_black] transition-shadow hover:underline hover:shadow-[1px_-2px_black]"
@@ -38,24 +25,17 @@
         href="https://www.linkedin.com/in/matt-jones123/"
         aria-label="Matt Jones' linkedin"
       >
-        <!-- Linkedin icon (lucide) -->
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
           viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          fill="currentColor"
           aria-hidden="true"
         >
           <path
-            d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+            d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
           />
-          <rect width="4" height="12" x="2" y="9" />
-          <circle cx="4" cy="4" r="2" />
         </svg>
       </a>
       <a
