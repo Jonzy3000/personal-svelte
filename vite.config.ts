5d4aeec597fd78a3eb14a45f62fb22c22ceb34a1
jonzy3000 <matt.r.jones1995@gmail.com>
2026-05-29 10:06:04 +0100

    chore: configure adapter-vercel, $components alias, and Vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import svg from '@poppanator/sveltekit-svg';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+	plugins: [enhancedImages(), sveltekit(), tailwindcss(), svg()]
+});
